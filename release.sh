4087b21161e0755ca78905824b973b5e78c72475
Ko-dany <dahyun.ko@acceldata.io>
2026-06-29 16:49:09 -0400

    ODP-6879: Update ODP dependencies

--- release.sh
+++ release.sh
@@ -9,10 +9,9 @@ fi
 echo "Release version: $RELEASE_VERSION"
 
 # update version in main pom.xml 
-sed -i "s|<clickhouse-java.version>.*<\/clickhouse-java.version>|<clickhouse-java.version>${RELEASE_VERSION}<\/clickhouse-java.version>|g" pom.xml
+sed -i '' "s|<clickhouse-java.version>.*</clickhouse-java.version>|<clickhouse-java.version>${RELEASE_VERSION}</clickhouse-java.version>|g" pom.xml
 
-# udpate examples with new version 
-find ./examples/ -type f -name "pom.xml" -exec sed -i "s|<clickhouse-java.version>.*<\/clickhouse-java.version>|<clickhouse-java.version>${RELEASE_VERSION}-SNAPSHOT<\/clickhouse-java.version>|g" '{}' \;
-find ./examples/ -type f -name "gradle.properties" -exec sed -i "s|^ch_java_client_version=.*$|ch_java_client_version=${RELEASE_VERSION}|g" '{}' \;
-find ./performance/ -type f -name "pom.xml" -exec sed -i "s|<ch.jdbc.revision>.*<\/ch.jdbc.revision>|<ch.jdbc.revision>${RELEASE_VERSION}-SNAPSHOT<\/ch.jdbc.revision>|g" '{}' \;
-
+# update examples with new version
+find ./examples/ -type f -name "pom.xml" -exec sed -i '' "s|<clickhouse-java.version>.*</clickhouse-java.version>|<clickhouse-java.version>${RELEASE_VERSION}</clickhouse-java.version>|g" '{}' \;
+find ./examples/ -type f -name "gradle.properties" -exec sed -i '' "s|^ch_java_client_version=.*$|ch_java_client_version=${RELEASE_VERSION}|g" '{}' \;
+find ./performance/ -type f -name "pom.xml" -exec sed -i '' "s|<ch.jdbc.revision>.*</ch.jdbc.revision>|<ch.jdbc.revision>${RELEASE_VERSION}</ch.jdbc.revision>|g" '{}' \;
